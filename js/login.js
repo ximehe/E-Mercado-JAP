bcb6f8b9bc72085545d2be30afe2d6570983d85b
Ximena <110623872+ximehe@users.noreply.github.com>
2022-08-20 12:06:42 -0300

    Update login.js

--- js/login.js
+++ js/login.js
@@ -3,9 +3,11 @@ document.getElementById("ok").addEventListener("click", function(){
     const email= document.getElementById("email").value;
     const password = document.getElementById("pass").value;
 
-    if (email && password){
+   if ((email && (password.length > 5))){
         window.location = "index.html";
     } else {
+        alert("Faltan datos.")
         window.location = "login.html";
     }
+
 })
