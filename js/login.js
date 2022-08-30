283543c0528c2dd68af7d62125d23dd5e9f6f7e8
Ximena <110623872+ximehe@users.noreply.github.com>
2022-08-30 00:14:44 -0300

    Update login.js

--- js/login.js
+++ js/login.js
@@ -4,9 +4,9 @@ document.getElementById("ok").addEventListener("click", function(){
     const password = document.getElementById("pass").value;
 
    if ((email && (password.length > 5))){
-        window.location = "index.html";
-       localStorage.setItem('userLog', email);
        sessionStorage.setItem('clave','logueado');
+       localStorage.setItem('userLog', email);
+       window.location = "index.html";
     } else {
         alert("No se pudo ingresar, revise si todos los espacios están llenos o si la contraseña tiene 6 o más caracteres ")
         window.location = "login.html";
